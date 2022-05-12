3ad96feb2d8a035c0a6cec0c4b505714e36f728a
Egor Bokov <s1lice.egor@gmail.com>
2022-05-12 14:58:50 +0300

    Project uploaded

---
+++ src/views/App/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {useToDoStore} from '../../data/stores/useToDoStore';
+import { InputPlus } from '../components/InputPlus';
+import { InputTask } from '../components/InputTask';
+import styles from './index.module.scss';
+
+export const App: React.FC = () => {
+    console.log(useToDoStore);
+
+    const [
+        tasks,
+        createTask,
+        updateTask,
+        removeTask
+    ] = useToDoStore(state => [
+        state.tasks,
+        state.createTask,
+        state.updateTask,
+        state.removeTask
+    ]);
+
+    console.log(tasks);
+
+    return (
+        <article className={styles.article}>
+            <h1 className={styles.articleTitle}>ToDo App</h1>
+            <section className={styles.articleSection}>
+                <InputPlus
+                    onAdd={title => {
+                        if (title) {
+                            createTask(title);
+                        }
+                    }}
+                />
+            </section>
+            <section className={styles.articleSection}>
+                {!tasks.length && (
+                    <p className={styles.articleText}>
+                        There is no one task!
+                    </p>
+                )}
+                {
+                    tasks.map(task => (
+                        <InputTask
+                            key={task.id}
+                            id={task.id}
+                            title={task.title}
+                            onDone={removeTask}
+                            onEdited={updateTask}
+                            onRemoved={removeTask}
+                        />
+                    ))
+                }
+            </section>
+        </article>
+    )
+}
